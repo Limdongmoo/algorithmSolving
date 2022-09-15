e377ec8efd8d34f37adaf1d575a7f0c9fc2e28a6
Limjongho <whdgh9873@icloud.com>
2022-09-15 19:17:09 +0900

    프로그래머스 LV.2 JadenCase 문자열 만들기 solve

---
+++ src/JadenCase/Solution.java
@@ -0,0 +1,26 @@
+package JadenCase;
+
+import java.util.stream.IntStream;
+
+class Solution {
+    public static String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean t = true;
+
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == ' ') {
+                sb.append(" ");
+                t = true;
+            } else {
+                if (t) {
+                    sb.append(Character.toUpperCase(c));
+                } else {
+                    sb.append(Character.toLowerCase(c));
+                }
+                t = false;
+            }
+        }
+        return sb.toString();
+    }
+}
